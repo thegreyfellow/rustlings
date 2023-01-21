8d1bc547364377433ab824e7eb8155369877be75
blank <youssef.idmoussi@gmail.com>
2023-01-21 04:14:18 +0100

    Add progress so far.

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,6 +11,14 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // Recursive solution:
+    // if num == 0 {
+    //     1
+    // } else {
+    //     factorial(num - 1) * num
+    // }
+    // fold and rfold
+    (1..=num).into_iter().rfold(1, |fac, n| fac * n)
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -10,8 +10,6 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -34,7 +32,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values()
+        .fold(0, |count, p| if p == &value { count + 1 } else { count })
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -53,7 +52,14 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection.iter().fold(0, |outer_count, collection| {
+        // outer_count
+        //     + collection
+        //         .values()
+        //         .into_iter()
+        //         .fold(0, |count, p| if p == &value { count + 1 } else { count })
+        outer_count + count_iterator(collection, value)
+    })
 }
 
 #[cfg(test)]
